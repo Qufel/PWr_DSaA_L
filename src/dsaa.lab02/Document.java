02b31f1f30e216465483298322bcab3c57c84ec6
Qufel <97023524+Qufel@users.noreply.github.com>
2025-03-16 19:28:03 +0100

    List No.2

--- src/dsaa.lab02/Document.java
+++ src/dsaa.lab02/Document.java
@@ -1,25 +1,61 @@
 package dsaa.lab02;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Document{
 	public String name;
 	public OneWayLinkedList<Link> links;
 	public Document(String name, Scanner scan) {
-			// TODO
-			load(scan);
-		}
-	public void load(Scanner scan) {
-		//TODO
+		this.name = name;
+		this.links=new OneWayLinkedList<>();
+		load(scan);
 	}
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
+	public void load(Scanner scan) {
+		String line = scan.nextLine();
+
+		// Ask for input until "eod" command
+		while (!line.equals("eod")) {
+
+			String[] words = line.split(" "); // Separate each line into words
+
+			// Loop through all words in a line
+			for (String word : words) {
+
+				// Check if a word is correct link
+				if (correctLink(word)) {
+
+					// Extract an identifier and add it to the list
+					String identifier = word.substring("link=".length()).toLowerCase();
+					links.add(new Link(identifier));
+
+				}
+			}
+
+			line = scan.nextLine();
+		}
+	}
+	// accepted only small letters, Capital letter, digits nad '_' (but not on the beginning)
 	private static boolean correctLink(String link) {
-		return true;
+		link = link.toLowerCase(); // Convert to lower case
+		return link.matches("link=[a-z][a-z0-9_]*");
 	}
 	
 	@Override
 	public String toString() {
-		return null;
+
+		StringBuilder str = new StringBuilder();
+
+		str.append("Document: ").append(name);
+
+		Iterator<Link> it = links.iterator();
+
+		while (it.hasNext()) {
+			Link link = it.next();
+			str.append("\n").append(link.ref);
+		}
+
+		return str.toString();
 	}
 
 }
--- src/dsaa.lab02/OneWayLinkedList.java
+++ src/dsaa.lab02/OneWayLinkedList.java
@@ -17,26 +17,29 @@ public class OneWayLinkedList<E> implements IList<E>{
 	Element sentinel;
 	
 	private class InnerIterator implements Iterator<E>{
-		// TODO
+
+		Element current;
+
 		public InnerIterator() {
-			// TODO
+			this.current = sentinel;
 		}
 		@Override
 		public boolean hasNext() {
-			// TODO
-			return false;
+			return current.next != null;
 		}
 		
 		@Override
 		public E next() {
-			// TODO
-			return null;
+
+			Element next = current.next;
+			current = next;
+			return next.object;
+
 		}
 	}
 	
 	public OneWayLinkedList() {
-		// make a sentinel	
-		// TODO
+		this.sentinel = new Element(null);
 	}
 
 	@Override
@@ -49,70 +52,255 @@ public class OneWayLinkedList<E> implements IList<E>{
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Adds an element to the end of the list.
+	 * @param e an element that will be added
+	 * @return {@code true} if value was added
+	 */
 	@Override
 	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
+		Element newElement = new Element(e);
+
+		if (isEmpty()) {
+			sentinel.next = newElement;
+		} else {
+
+			Element last = sentinel.next;
+
+			while (last.next != null) {
+				last = last.next;
+			}
+
+			last.next = newElement;
+		}
+
+		return true;
 	}
 
+	/**
+	 * Adds a given element at the provided index.
+	 * @param index an index where the element will be added
+	 * @param element an element that will be added
+	 * @throws NoSuchElementException
+	 */
 	@Override
 	public void add(int index, E element) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		
+		if (index < 0 || index > size()) throw new NoSuchElementException();
+
+		Element current = sentinel;
+
+		int i = 0;
+
+		while (current != null) {
+
+			if (i == index) {
+
+				Element newElement = new Element(element);
+
+				newElement.next = current.next;
+				current.next = newElement;
+
+			}
+
+			current = current.next;
+
+			i++;
+		}
 	}
 
+	/**
+	 * List is cleared by setting next element in sentinel to {@code null}.
+	 */
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		sentinel.next = null;
 	}
 
+	/**
+	 * @param element an element for which we are searching
+	 * @return {@code true} if searched element is found
+	 */
 	@Override
 	public boolean contains(E element) {
-		// TODO Auto-generated method stub
+		if (isEmpty()) return false;
+
+		InnerIterator it = new InnerIterator();
+
+		while (it.hasNext()) {
+			if (it.next().equals(element)) return true;
+		}
+
 		return false;
 	}
 
+	/**
+	 * @param index
+	 * @return {@link E} {@code element} an element of given index
+	 * @throws NoSuchElementException
+	 */
 	@Override
 	public E get(int index) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) throw new NoSuchElementException();
+		if (index < 0 || index >= size()) throw new NoSuchElementException();
+
+		InnerIterator it = new InnerIterator();
+
+		int i = 0;
+
+		E element = it.next();
+
+		while (it.hasNext()) {
+			if (i == index) break;
+			element = it.next();
+			i++;
+		}
+
+		return element;
 	}
 
+	/**
+	 * Sets a given element at the given index.
+	 * @param index an index where to set an element
+	 * @param element an element that will be set
+	 * @return {@link E} {@code element} previous element in the given index
+	 * @throws NoSuchElementException
+	 */
 	@Override
 	public E set(int index, E element) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) throw new NoSuchElementException();
+		if (index < 0 || index >= size()) throw new NoSuchElementException();
+
+		Element current = sentinel;
+		int i = 0;
+
+		E e = null;
+
+		while (current != null) {
+			if (i == index) {
+				e = current.next.object;
+				current.next.object = element;
+			}
+			current = current.next;
+			i++;
+		}
+
+		return e;
 	}
 
+	/**
+	 * @param element an element for which we're looking for an index of it
+	 * @return {@code index} an index of an element, {@code -1} if no index found
+	 */
 	@Override
 	public int indexOf(E element) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isEmpty()) return -1;
+
+		int index = 0;
+
+		InnerIterator it = new InnerIterator();
+
+		E e;
+
+		while (it.hasNext()) {
+			e = it.next();
+			if (e.equals(element)) {
+				return index;
+			}
+			index++;
+		}
+
+		return -1;
 	}
 
+	/**
+	 * @return {@code true} if sentinel has no next element
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return sentinel.next == null;
 	}
 
+	/**
+	 * @param index
+	 * @return {@link E} {@code element} a removed element
+	 * @throws NoSuchElementException
+	 */
 	@Override
 	public E remove(int index) throws NoSuchElementException {
-		// TODO Auto-generated method stub
+		if (isEmpty()) throw new NoSuchElementException();
+		if (index < 0 || index >= size()) throw new NoSuchElementException();
+
+		int i = 0;
+
+		Element previous = sentinel;
+		Element element = sentinel.next;
+
+		while (element != null) {
+
+			if (i == index) {
+
+				E e = element.object;
+
+				previous.next = element.next;
+
+				return e;
+			}
+
+			previous = element;
+			element = element.next;
+
+			i++;
+		}
+
 		return null;
 	}
 
+	/**
+	 * Removes the first occurrence of a given element.
+	 * @param e an element to be removed
+	 * @return {@code true} if element was removed successfully
+	 */
 	@Override
 	public boolean remove(E e) {
-		// TODO Auto-generated method stub
+
+		if (isEmpty()) return false;
+		if (!contains(e)) return false;
+
+		InnerIterator it = new InnerIterator();
+
+		E element;
+
+		while (it.hasNext()) {
+			element = it.next();
+			if (element.equals(e)) {
+
+				remove(indexOf(element));
+				return true;
+
+			}
+		}
+
 		return false;
 	}
 
+	/**
+	 * @return the size of a list
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		if (isEmpty()) return 0;
+
+		int size = 0;
+
+		InnerIterator it = new InnerIterator();
+
+		while (it.hasNext()) {
+			it.next();
+			size++;
+		}
+
+		return size;
 	}
 	
 }
